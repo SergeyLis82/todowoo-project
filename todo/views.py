4b3294b8eebb54723107596ffbf50d10dd89f597
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2021-12-09 15:40:32 +0300

    Add form

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
-# Create your views here.
+def signup_user(request):
+    content = {
+        'title': "SignUp",
+        'heading': "Sign Up",
+        'form': UserCreationForm(),
+    }
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', content)
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('current_todos')
+            except IntegrityError:
+                content['error'] = "User is already exists."
+                return render(request, 'todo/signupuser.html', content)
+        else:
+            content['error'] = "Password didn't match"
+            return render(request, 'todo/signupuser.html', content)
+
+def current_todos(request):
+    content = {
+        'title': "ToDo's",
+        'heading': " - todo's",
+    }
+    return render(request, 'todo/currenttodos.html', content)
